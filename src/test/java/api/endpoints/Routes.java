55ef8359337cc4e4434bd9873743d293277cdf15
Ashitosh Kole <ashitoshkole007@gmail.com>
2025-08-17 23:34:03 +0530

    updated on 17-08-25

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,17 @@
+package api.endpoints;
+
+
+
+public class Routes {
+
+	public static String baseurl = "https://petstore.swagger.io/v2";
+	
+	public static String post_url = baseurl + "/user";
+	public static String get_url = baseurl + "/user/{username}";
+	public static String update_url = baseurl + "/user/{username}";
+	public static String delete_url = baseurl + "/user/{username}";
+	
+	
+	
+	
+}
